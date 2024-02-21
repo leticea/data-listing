29ac3338c715988973928dfda904317e0524a60e
leticea <nascimento.leticeah@gmail.com>
2024-02-21 19:51:03 -0300

    feat(app.tsx): inserting components for styling

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,14 @@
 import { Plus } from "lucide-react";
+import { Header } from "./components/header";
+import { Tabs } from "./components/tabs";
 
 export function App() {
   return (
     <div className="py-10 space-y-8">
-      <div>header tabs</div>
+      <div>
+        <Header />
+        <Tabs />
+      </div>
       <main className="max-w-6xl mx-auto space-y-5">
         <div className="flex items-center gap-3">
           <h1 className="text-xl font-bold">Tags</h1>
