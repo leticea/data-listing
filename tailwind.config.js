31d364ae7be20f3843b8bbef3cf71f4f9d3211cd
leticea <nascimento.leticeah@gmail.com>
2024-02-21 19:27:55 -0300

    feat(index.html)first commit: creating the project

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./src/**/*.tsx", "./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: "Inter, sans-serif",
+      },
+    },
   },
   plugins: [],
 };
