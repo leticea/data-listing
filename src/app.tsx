6f86f311645d13dc7367dc3d73d2c822826fb139
leticea <nascimento.leticeah@gmail.com>
2024-02-21 19:35:19 -0300

    feat(app.tsx): creating styles

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,16 @@
+import { Plus } from "lucide-react";
+
 export function App() {
-  return <h1>Hello world</h1>;
+  return (
+    <div className="py-10 space-y-8">
+      <div>header tabs</div>
+      <main className="max-w-6xl mx-auto space-y-5">
+        <h1 className="text-xl font-bold">Tags</h1>
+        <button>
+          <Plus className="size-3" />
+          Create new
+        </button>
+      </main>
+    </div>
+  );
 }
