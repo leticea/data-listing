82833129d28fc8bf25e23c29167af01a5401ff36
leticea <nascimento.leticeah@gmail.com>
2024-02-22 20:32:57 -0300

    feat(app.tsx): inserting KeepPreviousData

--- src/app.tsx
+++ src/app.tsx
@@ -14,6 +14,8 @@ import {
 import { Pagination } from "./components/pagination";
 import { useQuery, keepPreviousData } from "@tanstack/react-query";
 import { useSearchParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import useDebounceValue from "./hooks/use-debounce-value";
 
 export interface TagResponse {
   first: number;
@@ -33,14 +35,17 @@ export interface Tag {
 
 export function App() {
   const [searchParams] = useSearchParams();
+  const [filter, setFilter] = useState("");
+
+  const debouncedFilter = useDebounceValue(filter, 1000);
 
   const page = searchParams.get("page") ? Number(searchParams.get("page")) : 1;
 
   const { data: tagsResponse, isLoading } = useQuery<TagResponse>({
-    queryKey: ["get-tags", page],
+    queryKey: ["get-tags", debouncedFilter, page],
     queryFn: async () => {
       const response = await fetch(
-        `http://localhost:3333/tags?_page=${page}&_per_page=10`
+        `http://localhost:3333/tags?_page=${page}&_per_page=10&title=${debouncedFilter}`
       );
       const data = await response.json();
 
@@ -73,7 +78,11 @@ export function App() {
         <div className="flex items-center justify-between">
           <Input variant="filter">
             <Search className="size-3" />
-            <Control placeholder="Search tags..." />
+            <Control
+              placeholder="Search tags..."
+              onChange={(e) => setFilter(e.target.value)}
+              value={filter}
+            />
           </Input>
 
           <Button>
