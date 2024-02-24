d7899e5079050b9073061309d7a4c83c8a82a556
leticea <nascimento.leticeah@gmail.com>
2024-02-24 16:20:48 -0300

    feat(create-tag-form.tsx): input style

--- src/components/create-tag-form.tsx
+++ src/components/create-tag-form.tsx
@@ -3,15 +3,28 @@ import { Button } from "./ui/button";
 
 export function CreateTagForm() {
   return (
-    <form className="w-full">
-      <div>
-        <label>Tag name</label>
-        <input type="text" />
+    <form className="w-full space-y-6">
+      <div className="space-y-2">
+        <label className="text-sm font-medium block" htmlFor="name">
+          Tag name
+        </label>
+        <input
+          id="name"
+          type="text"
+          className="border border-zinc-800 rounded-lg px-3 py-2 bg-zinc-800/50 w-full"
+        />
       </div>
 
-      <div>
-        <label>Slug</label>
-        <input type="text" readOnly />
+      <div className="space-y-2">
+        <label className="text-sm font-medium block" htmlFor="slug">
+          Slug
+        </label>
+        <input
+          id="slug"
+          type="text"
+          className="border border-zinc-800 rounded-lg px-3 py-2 bg-zinc-800/50 w-full"
+          readOnly
+        />
       </div>
 
       <div className="flex items-center justify-end gap-2">
