92aa0e5122c524d5d9c16b39ec7ee609df49d0e5
leticea <nascimento.leticeah@gmail.com>
2024-02-24 16:10:37 -0300

    feat(create-tag-form.tsx): button style

--- src/components/create-tag-form.tsx
+++ src/components/create-tag-form.tsx
@@ -1,9 +1,9 @@
-import { Check } from "lucide-react";
+import { Check, X } from "lucide-react";
 import { Button } from "./ui/button";
 
 export function CreateTagForm() {
   return (
-    <form>
+    <form className="w-full">
       <div>
         <label>Tag name</label>
         <input type="text" />
@@ -14,9 +14,12 @@ export function CreateTagForm() {
         <input type="text" readOnly />
       </div>
 
-      <div>
-        <Button>Cancel</Button>
-        <Button type="submit">
+      <div className="flex items-center justify-end gap-2">
+        <Button>
+          <X className="size-3" />
+          Cancel
+        </Button>
+        <Button className="bg-teal-400 text-teal-950" type="submit">
           <Check className="size-3" />
           Save
         </Button>
