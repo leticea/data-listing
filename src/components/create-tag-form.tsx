fc18fe9a632c29672ec85e46a0acd080c2fbebb9
leticea <nascimento.leticeah@gmail.com>
2024-02-24 16:06:33 -0300

    feat(create-tag-form.tsx): inserting input and button

--- src/components/create-tag-form.tsx
+++ src/components/create-tag-form.tsx
@@ -1,3 +1,26 @@
+import { Check } from "lucide-react";
+import { Button } from "./ui/button";
+
 export function CreateTagForm() {
-  return <h1>Hello World</h1>;
+  return (
+    <form>
+      <div>
+        <label>Tag name</label>
+        <input type="text" />
+      </div>
+
+      <div>
+        <label>Slug</label>
+        <input type="text" readOnly />
+      </div>
+
+      <div>
+        <Button>Cancel</Button>
+        <Button type="submit">
+          <Check className="size-3" />
+          Save
+        </Button>
+      </div>
+    </form>
+  );
 }
